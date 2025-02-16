1d2cd7134083ba9d013f5296216f53ecb67cbb39
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-02-16 21:36:47 +0530

    Ioc in main mod

--- src/main/java/com/excelr/Main.java
+++ src/main/java/com/excelr/Main.java
@@ -7,12 +7,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Ioc container starts (ctlr+space)
 		ApplicationContext con=new ClassPathXmlApplicationContext("config.xml");
 		
-		
+		//getBean called
 		
 		Student res=con.getBean("s1",Student.class);
-		
+		System.out.println(res);
 
 	}
 
